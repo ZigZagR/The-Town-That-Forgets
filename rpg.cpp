4a6ef665c4a9bf46d6f1e3ea71cb87714f5713b3
Zig <dudanunesgoncalves@gmail.com>
2026-04-07 21:31:08 -0300

    Mundo dos espelhos começo

--- rpg.cpp
+++ rpg.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 // nome
-std::string nome = "V\u016B\u0119rg.";
+std::string nome = "Gustavo";
 
 // items
 bool temRelogioVerdadeiro = false;
@@ -23,6 +23,8 @@ bool matouReflexo = false;
 bool caiuBuraco = false;
 bool comido = false;
 bool homemExiste = true;
+bool reflexoAutonomo = false;
+bool seguindoHomem = false;
 
 // história
 int viuReflexo = 0;
@@ -44,6 +46,7 @@ void fim_perfeito();
 void fim_saida();
 void fim_esquecido();
 void fim_npc();
+void creditos();
 
 // funções para ajudar 
 void printCreepy(); // printa com intervalos aleatórios
@@ -88,6 +91,7 @@ void printSuspense(string texto){
 }
 
 void intro() { 
+    system("cls"); // Limpa a tela do terminal
     cout << "O ônibus partiu sem você perceber. Ou talvez nem tenha parado para começar.\n";
     cout << "O ponto de ônibus permanece silencioso. Muito silencioso\n"; 
     cout << "A placa da parada está vazia, exceto pelo símbolo de um ônibus.\n";
@@ -185,7 +189,7 @@ void homem(){ // perdão pedro. me passei.
     
     } else if(interagiuHomem == 1){
         cout << "\n";
-        cout << "Ele reage. Mas, não a você, e diz: Você não deveria estar ainda aqui.\n";
+        cout << "Ele reage. Mas, não a você, e diz: Você não deveria estar aqui ainda.\n";
         cout << "\n";
         interagiuHomem++;
 
@@ -210,11 +214,11 @@ void homem(){ // perdão pedro. me passei.
         cout << "\n";
 
         int escolha;
-        escolha = verificaInput(2);
+        escolha = verificaInput(3);
 
         if(escolha == 1){
             cout << "Ele sorri e diz: \n";
-            cout << "Eu sou você, mas você não ainda não é Eu. \n";
+            cout << "Eu sou você, mas você ainda não é Eu. \n";
             cout << "1. Perguntar quem é você. \n";
             cout << "2. Perguntar onde você está. \n";
             cout << "3. Perguntar como sair da cidade. \n";
@@ -225,9 +229,8 @@ void homem(){ // perdão pedro. me passei.
             if(escolha2 == 1){
                 cout << "\n";
                 cout << "O sorriso dele fecha e ele diz: \n";
-                string teste = "Olá Hai Hai";
-                printSuspense(teste);
-                //cout << "V\u016B\u0119rg. \n";
+
+                printSuspense(nome);
                 cout << "Você lembra o seu nome. O homem desaparece. Você está sozinho novamente.\n";
                 cout << "Mas há um relógio de bolso em sua mão. Ele está funcionando\n";
                 cout << "\n";
@@ -281,7 +284,9 @@ void homem(){ // perdão pedro. me passei.
             int escolha2;
             escolha2 = verificaInput(2);
             
-            if(escolha2 == 1){
+             if(escolha2 == 1){
+                seguindoHomem = true;
+                homemExiste = false;
                 beco();
             }else if(escolha2 == 2){
                 ruas();
@@ -309,26 +314,39 @@ void homem(){ // perdão pedro. me passei.
 void reflexo(){
 
     if(lembraNome || memoriaInstavel){
-        cout << "Você se sente compelido a olhar o seu reflexo. Ele parece diferente. Você perecebe que aquilo não pode ser você\n";
-        cout << "\n";
-        cout << "1. Observar.\n";
-        cout << "2. Tocar o vidro.\n";
-        cout << "3. Voltar.\n";
+        if(matouReflexo == false){
+            cout << "Você se sente compelido a olhar o seu reflexo. Ele parece diferente. Você perecebe que aquilo não pode ser você\n";
+            cout << "1. Observar.\n";
+            cout << "2. Tocar o vidro.\n";
+            cout << "3. Voltar.\n";
 
-        int escolha;
-        escolha = verificaInput(3);
-        
-        if(escolha == 1){
-            matouReflexo = true;
-            cout << "Como a chama de uma vela, você percebe subitamente. Você não está lá. Nunca esteve. Em seu lugar... Nada sempre houve.\n";
-            cout << "\n";
-            cout << "O reflexo já não mais ali mostra apenas o que deveria estar ali. Você não está no reflexo.\n";
-            cout << "Você está nas ruas novamente.";
-            
-        }else if(escolha == 2){
-            espelho();
-        }else if(escolha == 3){
-            ruas();
+            int escolha;
+            escolha = verificaInput(3);
+
+            if(escolha == 1){
+                matouReflexo = true;
+                cout << "Como a chama de uma vela, você percebe subitamente. Você não está lá. Nunca esteve. Em seu lugar... Nada sempre houve.\n";
+                cout << "O reflexo já não mais ali mostra apenas o que deveria estar ali. Você não está no reflexo.\n";
+                reflexo();
+
+            }else if(escolha == 2){
+                espelho();
+            }else if(escolha == 3){
+                ruas();
+            }
+        }else{
+            cout << "Você novamente está a só na frente do vidro da loja. Ele reflete o céu, as lojas e as ruas. Mas não você.\n";
+            cout << "1. Tocar o vidro.\n";
+            cout << "2. Voltar.\n";
+
+            int escolha;
+            escolha = verificaInput(2);
+
+            if(escolha == 1){
+                espelho();
+            }else if(escolha == 2){
+                ruas();
+            }
         }
     }
 
@@ -362,7 +380,8 @@ void reflexo(){
             ruas();
         }
     }else if(viuReflexo == 2){
-        cout << "Você observa seu reflexo no espelho. Você move suas mãos, mas o reflexo, sorri.";
+        cout << "Você observa seu reflexo no espelho. Você move suas mãos, mas o reflexo, sorri. \n";
+        reflexoAutonomo = true;
         viuReflexo++;
         memoriaInstavel = true;
         
@@ -402,17 +421,20 @@ void saidaDaCidade(){
             ruas();
         }
 
-    }else if(memoriaInstavel == false && fugiuCidade == 2){
+    }else if(memoriaInstavel == true && fugiuCidade == 2){
         fim_saida();
 
-    }else if(memoriaInstavel == true && fugiuCidade == 2){
+    }else if(memoriaInstavel == false && fugiuCidade == 2){
 
-        cout << "Você continua a andar. E andar. E andar. E andar.";
-        cout << "Sua memória já não estava boa no centro da cidade. Algumas coisas já pareciam fora de lugar. Havia uma loja somente de roupas e acessórios amarelas na cidade.\n";
+        cout << "Você continua a andar. E andar. E andar. E andar.\n";
+        cout << "Sua memória já não estava boa no centro da cidade. Algumas coisas já pareciam fora de lugar.\n";
+        cout << "Havia uma loja somente de roupas e acessórios amarelas na cidade.\n";
         cout << "Você juraria que ela nunca estava lá, mas já não tem mais tanta certeza.\n";
         cout << "Há um homem no fim da rua. Um homem em amarelo. Terno amarelo. Chapéu amarelo. Sapatos amarelos. Bengala amarela.\n";
-        cout << "Você não sabe se ele estava ali ou não. As vezes ao olhar ele está lá. As vezes ao piscar ele está lá. As vezes ao abrir os olhos depois de fechar ele não está mais.\n";
-        cout << "A última vez que você vê o homem, ele sorri. Ele já desapareceu por um tempo. Você já não sabe mais quanto tempo andou para ter uma referência.\n";
+        cout << "Você não sabe se ele estava ali ou não. As vezes ao olhar ele está lá. As vezes ao piscar ele está lá.\n";
+        cout << "As vezes ao abrir os olhos depois de fechar ele não está mais.\n";
+        cout << "A última vez que você vê o homem, ele sorri. Ele já desapareceu por um tempo.\n";
+        cout << "Você já não sabe mais quanto tempo andou para ter uma referência.\n";
         cout << "Você continua a andar em frente, a cidade já não estava mais atrás de você.\n";
         caiuBuraco = true;
         fim_esquecido();
@@ -420,19 +442,131 @@ void saidaDaCidade(){
 }
 
 void espere(){
+    if(esperou == 0){
+        cout << "Você espera por um tempo. Nada muda. O tempo passa. E passa. E passa. Você percebe um beco com uma iluminação precária\n"; 
+        esperou++;
+        
+        cout << "1. Tentar novamente.\n";
+        cout << "2. Voltar.\n";
+        int escolha;
+        escolha = verificaInput(2);
 
-    cout << "Em dev.\n"; // ACABAR
-    esperou++;
-    ruas();
+        if(escolha == 1){
+            espere();
+        }else if(escolha == 2){
+            ruas();
+        }
+
+    }else if(esperou == 1){
+        cout << "Você continua a esperar. O homem continua a andar desaparecendo no horizonte e reaparecendo eventualmente.\n";
+        cout << "O reflexo nas lojas continua o mesmo. O beco permanece parece mais atrativo com o passar do tempo.\n";
+        memoriaInstavel = true;
+        esperou++;
+
+        cout << "1. Tentar novamente.\n";
+        cout << "2. Voltar.\n";
+        int escolha;
+        escolha = verificaInput(2);
+
+        if(escolha == 1){
+            espere();
+        }else if(escolha == 2){
+            ruas();
+        }
+
+    }else if(memoriaInstavel == true && esperou == 2){
+        cout << "Você se pega andando até o beco. Você não se lembra de ter decidido andar até o beco. Mas você está lá.\n";
+        beco();
+    }
 }
 
-void beco(){
-    
+void beco(){  
+    if(seguindoHomem){
+        cout << "Você segue o homem para dentro do beco. Apenas a luz de uma lâmpada pendurada em um fio de metal ilumina o caminho.\n";
+        cout << "Você não consegue ver nada a sua frente exceto a vaga imagem das costas do homem a sua frente.\n";
+        cout << "Você não consegue ver o fim do beco. A escuridão é densa.\n";
+        cout << "Você não consegue ver o começo do beco. A escuridão é densa.\n";
+        cout << "Você não consegue ver o que está atrás de você. A escuridão é densa.\n";
+        cout << "Você não consegue ver o que está na frente de você. A escuridão é densa.\n";
+        cout << "Você não consegue ver o que está acima de você. A escuridão é densa.\n";
+        cout << "Você não consegue ver o que está abaixo de você. A escuridão é densa.\n";
+        cout << "Você não consegue ver nada. A escuridão é densa.\n";
+        cout << "Você não consegue ver nada. A escuridão é densa.\n";
+        cout << "De repente. Uma luz se ascende na escuridão.\n";
+        cout << "Há uma luz pendurada, balançando levemente no que parece ser o fim do beco.\n";
+        cout << "O homem continua andando, alheio a luz.\n";
+        cout << "Você continua andando atrás dele, o homem então atravessa poucos passos após a luz e desaparece.\n";
+        cout << "Você está sozinho no beco.\n";
+        
+        cout << "1. Ir até a luz.\n";
+        cout << "2. Voltar.\n";
+        int escolha;
+        escolha = verificaInput(2);
+
+        if(escolha == 1){
+            fim_esquecido();
+        }else if(escolha == 2){
+            ruas();
+        }
+    }
+    if(memoriaInstavel == true && esperou == 2){
+        cout << "Você está no beco de fato. As paredes são de tijolos de pedra assim como todo o resto da cidade.\n";
+        cout << "A iluminação vem de uma lâmpada pendurada por um fio de metal. O fio balança de um lado para o outro.\n";
+        cout << "Você não consegue ver o fim do beco. A escuridão é densa.\n";
+        cout << "Você não consegue ver o começo do beco. A escuridão é densa.\n";
+        cout << "Você não consegue ver o que está atrás de você. A escuridão é densa.\n";
+        cout << "Você não consegue ver o que está na frente de você. A escuridão é densa.\n";
+        cout << "Você não consegue ver o que está acima de você. A escuridão é densa.\n";
+        cout << "Você não consegue ver o que está abaixo de você. A escuridão é densa.\n";
+        cout << "Você não consegue ver nada. A escuridão é densa.\n";
+        cout << "Você não consegue ver nada. A escuridão é densa.\n";
+        cout << "De repente. Uma luz se ascende na escuridão.\n";
+        cout << "Há uma luz pendurada, balançando levemente no que parece ser o fim do beco.\n";
+        
+        cout << "1. Ir até a luz.\n";
+        cout << "2. Voltar.\n";
+        int escolha;
+        escolha = verificaInput(2);
+
+        if(escolha == 1){
+            comido = true;
+            fim_esquecido();
+        }else if(escolha == 2){
+            ruas();
+        }
+    }    
     cout << "Em dev.\n"; // ACABAR
     ruas();
 }
 
 void espelho(){
+    // Plano  Espelho)    
+    // - Corredor de Reflexos 
+    // 1. Imitar o reflexo - Perde controle de si por uns segundos, se repetir pode trocar de lugar com o reflexo
+    // 2. Ignorar e seguir - Segue em frente, seu reflexo fica para trás chegando em uma sala central
+    // 3. Tocar o espelho - O reflexo sai do espelho e começa a andar para alguma direção desconhecida parecendo incompleto ou sem senso de direção
+    // 4. Observar por mais tempo - O reflexo começa a agir sozinho com o tempo que passa. Eventualmente ele some. // rota secreta TRIGGER insanidade < 10
+
+    // - Sala das Versões - Versão cega, surda e muda
+    // 1. Gesticular para o cego. - Ilusão de escolha vai os 3 pro mesmo canto, inferno. 
+    // 2. Falar com o surdo.
+    // 3. Tocar o mudo.
+    // 4. Ignorar todos e seguir em frente. - Não interagir com o mal te faz seguir em frente recuperando o estado mental talvez?.
+
+    // - Espaço Sem Reflexo
+    // 1. Esperar.
+    // 2. Seguir em frente.
+    // 3. Gritar.
+    // 4. Voltar.
+
+    // - Espelho Quebrado (rota secreta good ending) - Fragmentos de realidades diferentes
+    // 1. Fragmento de realidade A - Você vê a vida real. Rota de desvaneio romântico editada.
+    // 2. Fragmento de realidade B - Você vê a cidade da forma que ela realmente é. Um pequeno mundo no estômago de Azazoth 
+    // 3. Voltar.
+
+    // - Saída (não confiável)
+    // Você saí mas algo pode vir junto ou não e tomar o seu lugar 50/50
+
     cout << "Em dev.\n"; // ACABAR
     ruas();
 }
@@ -441,11 +575,14 @@ void fim_npc(){
     cout << "Você segue o homem até que ele anda para fora dos limites da cidade. Você tenta continuar a seguí-lo, mas você se pega no caminho contrário.\n";
     cout << "Apenas podendo ver as costas do homem andando livre pelos campos verdejantes dos arredores daquela cidade.\n";
     cout << "Você está sozinho novamente. Andando. E andando. E andando. E andando.\n";
-    cout << "Depois de um tempo, você se esquece que já fez outra coisa além de andar por aquela rua. Um dia uma pessoa que não estava esteja ali não se aproxima e não te segue.\n";
-    cout << "Você chega ao fim da sua rota, onde o homem que não estava ali antes de você cruzou a barreira final e saiu da cidade. Desta vez você não se pega retornando pelo mesmo caminho e segue em frente.\n";
-    cout << "Vendo a expressão horrorizada daquilo que não estava ali não conseguindo sair. Você continua a andar e chega em uma colina coberta de flores. Cessando sua existência e tornando-se uma delas.\n";
+    cout << "Depois de um tempo, você se esquece que já fez outra coisa além de andar por aquela rua.\n";
+    cout << "Um dia uma pessoa que não estava ali não se aproxima e não te segue.\n";
+    cout << "Você chega ao fim da sua rota, onde o homem que não estava lá antes de você nunca cruzou a barreira final e nunca saiu da cidade.\n";
+    cout << "Desta vez você não se pega retornando pelo mesmo caminho e segue em frente.\n";
+    cout << "Vendo a expressão horrorizada daquilo que não estava ali não conseguindo sair ao tentar segui-lo.\n";
+    cout << "Você continua a andar e chega em uma colina coberta de flores. Cessando sua existência e tornando-se uma delas.\n";
     cout << "Fim\n";
-    exit(0); // Finaliza
+    creditos();
 }
 
 void fim_esquecido(){
@@ -456,27 +593,44 @@ void fim_esquecido(){
         cout << "Você apenas cai. E cai. E cai. Você aterrisa em algo sólido. Não houve nenhum dano a você com a queda.\n";
     }
     if(comido){
-        cout << "Em dev.\n"; // ACABAR
+        cout << "Você anda em direção a luz. Mas quando percebe o cheiro de peixe e sangue já é tarde demais.\n";
+        cout << "Você é devorado por uma criatura similar a um peixe lanterna. Usando da luz para atrair suas presas.\n";
+        cout << "Há apenas escuridão por um bom tempo. Não há corrente de ar. Não há nada além de uma força de sucção para baixo.\n";
+        cout << "Você não consegue se lembrar de nada. Nem do seu nome. Nem do seu rosto. Nem do que você estava fazendo.\n";
+        cout << "Você apenas cai. E cai. E cai. Você aterrisa em algo sólido. Não houve nenhum dano a você com a queda.\n";
     }
 
     cout << "O local parece uma pequena ilha. No entanto o horizonte é somente uma massa completamente escura que nenhuma luz parece conseguir refletir.\n";
     cout << "Você fica ali naquela ilha de nada. Suas memórias não voltam. Para você, sempre só existiu aquela ilha de nada.\n";
     cout << "Você apenas existe. E existe. E existe. E existe. Esquecido pela cidade.\n";
     cout << "Fim\n";
-    exit(0); // Finaliza
+    creditos();
 }
 
 void fim_perfeito(){
     cout << "Em dev.\n"; // ACABAR
-    exit(0); // Finaliza
+    creditos();
 }
 
 void fim_saida(){
-    cout << "Você anda por bastante tempo. A cidade fica para trás de você. As coisas começam a ficar estranhas, pequenas inconsistências. Você já não sabe por quanto tempo andou.\n";
-    cout << "1 minuto? 10 horas? 100 dias? 1000 anos? Após um certo tempo, rachaduras começam a aparecer de canto de olho. Quando observadas novamente, elas não estão mais lá. \n";
-    cout << "Mas você sente algo ali. Observando-o. À espreita. NADA, você viu. As rachaduras aumentam. Dessa vez elas não desaparecem. A barreira que impedia o olhar daquilo se rompeu. \n";
-    cout << "De forma explosiva as rachaduras se montam e remontam, pulsando. Os olhares através do véu já não afetam mais sua mente. Sua mente já longe demais para ser humana. \n";
+    cout << "Você anda por bastante tempo. A cidade fica para trás de você. As coisas começam a ficar estranhas, pequenas inconsistências. \n";
+    cout << "Você já não sabe por quanto tempo andou. 1 minuto? 10 horas? 100 dias? 1000 anos? \n";
+    cout << "Após um certo tempo, rachaduras começam a aparecer de canto de olho. \n";
+    cout << "Quando observadas novamente, elas não estão mais lá. \n";
+    cout << "Mas você sente algo ali. Observando-o. À espreita. NADA, você viu. As rachaduras aumentam. \n"; 
+    cout << "Dessa vez elas não desaparecem. A barreira que impedia o olhar daquilo se rompeu. \n";
+    cout << "De forma explosiva as rachaduras se montam e remontam, pulsando. Os olhares através do véu já não afetam mais sua mente. \n";
+    cout << "Sua mente já longe demais para ser humana. \n";
     cout << "Uma pequena fechadura se abre à sua frente. Você saiu da cidade. Mas você nunca esteve nela. \n";
+    cout << "Fim.\n";
+    creditos();
+}
+
+void creditos(){
+    cout << "Créditos:\n";
+    cout << "Desenvolvedores: Luiz Franke e Pedro Henrique.\n";
+    cout << "Roteiro: Luiz Franke e Pedro Henrique.\n";
+    cout << "Menções honrosas: Alberto H.W. , Gasfer,  Gustavo de Borba Vigarani, Nicolas Gabriel Berenstein.\n";
     cout << "Fim.\n";
     exit(0); // Finaliza
 }
